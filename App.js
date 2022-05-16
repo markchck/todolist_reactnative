cf680e9beff4e2164e0114080ad0b3488e6d2acf
System Administrator <markchck@gmail.com>
2022-05-16 19:10:46 +0900

    deploy

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, ScrollView, Alert, Platform } from 'react-native';
 import { useState, useEffect } from 'react';
 import { theme } from './color';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Fontisto } from '@expo/vector-icons';
+import 'expo-dev-client';
 
 const STORAGE_KEY = "@toDos"
 
@@ -15,24 +16,13 @@ export default function App() {
   const work =   () => setWorking(true);
   const onChangeText = (payload)=> setText(payload);
 
-  const saveToDos = async(toSave) => {
-    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
-  }
-  const loadToDos = async()=>{
-    const s = await AsyncStorage.getItem(STORAGE_KEY)
-    // console.log(s, JSON.parse(s))
-    setToDos(JSON.parse(s))
-  }
-  useEffect(()=>{
-    loadToDos();
-  }, []);
-
   const addToDo = async () => {
+
     if(text === ""){
       return
     }
     const newToDos = Object.assign({}, toDos, { [Date.now()] : {text:text, working:working}, } )
-    // 이 코드가 왜 위에 코드랑 같은지 바로 이해가 안될 수 있는데 이거 A = A +1 과 같은 로직의 코드임.
+    // 아래 코드가 왜 위에 코드랑 같은지 바로 이해가 안될 수 있는데 이거 A = A +1 과 같은 로직의 코드임.
     // 다만 A와 똑같지는 않고 A`정도로 이해하면 되겠다. (리액트에서는 state를 직접 수정하는걸 금하기 때문에 setState를 수정하거나)
     // ...문법을 써서 A의 복제본인 A`을 만들어서 써야한다.
     // const newToDos = {
@@ -46,7 +36,31 @@ export default function App() {
     // console.log(toDos[1652104358532]);
     // console.log(Object.keys(toDos)[0]);
   }
+
+  const saveToDos = async(toSave) => {
+    await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(toSave))
+  }
+  const loadToDos = async()=>{
+    const s = await AsyncStorage.getItem(STORAGE_KEY)
+    // console.log(s, JSON.parse(s))
+    if(s){
+      setToDos(JSON.parse(s))
+    }
+  }
+  useEffect(()=>{
+    loadToDos();
+  }, []);
+
   const deleteToDo = async(res)=>{
+    if(Platform.OS === "web"){
+      const ok = confirm("Do you want delete? (In Web)")
+      if(ok){
+        const newToDos = {...toDos}
+        delete newToDos[res]
+        setToDos(newToDos);
+        saveToDos(newToDos)
+      }
+    }
     Alert.alert(
       "Delete To Do",
       "Are you sure?", 
@@ -55,26 +69,23 @@ export default function App() {
         {text: "I'm sure", onPress: ()=>{
           const newToDos = {...toDos}
           delete newToDos[res]
-          console.log(newToDos)
+          // console.log(newToDos)
           setToDos(newToDos);
           saveToDos(newToDos)
         } }        
       ]
     );
-
-
-    
   }  
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
       <View style={styles.header}>
         <TouchableOpacity onPress={work}>
-          <Text style={{...styles.btnText, color: working ? "white" : theme.grey }}>Work</Text>
+          <Text style={{fontSize: 33, fontWeight: "600", color: working ? "white" : theme.grey }}>Work</Text>
         </TouchableOpacity>
         
         <TouchableOpacity onPress={travel}>
-          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey }}>Travel</Text>
+          <Text style={{fontSize: 33, fontWeight: "600", color: !working ? "white" : theme.grey }}>Travel</Text>
         </TouchableOpacity>
       </View>
         <TextInput
@@ -86,7 +97,8 @@ export default function App() {
         />
         <ScrollView>
           {Object.keys(toDos).map((res)=>(
-            toDos[res].working === working? <View key={res} style={styles.toDo}>
+            toDos[res].working === working?
+            <View key={res} style={styles.toDo}>
               <Text style={styles.toDoText}>{toDos[res].text}</Text>
               <TouchableOpacity onPress={()=> deleteToDo(res)}>
                 <Fontisto name="trash" size={20} color="white" />
@@ -111,8 +123,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between"
   },
   btnText:{
-    fontSize: 33,
-    fontWeight: "600",
+    
   },
   input:{
     backgroundColor:"white",
